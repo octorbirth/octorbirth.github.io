69786f97439c7d4a261c7e1c4e228ca3e704fe44
octorbirth <hsu9643@gmail.com>
2018-04-16 16:26:14 +0900

    current time

--- js/main.js
+++ js/main.js
@@ -55,15 +55,32 @@ $(window).load(function(){
 /* END ------------------------------------------------------- */
 
 
-$('#countdown').countdown({
-	date: "December 13, 2017 18:03:26",
-	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-	}
-});
+
+function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+
+    if (n.length < digits) {
+        for (var i = 0; i < digits - n.length; i++)
+            zero += '0';
+    }
+    return zero + n;
+}
+
+
+var interval = setInterval( function() {
+    $('#nowTime').empty();
+
+
+    var now = new Date();
+    var H = now.getHours();
+    var M = now.getMinutes();
+    var S = now.getSeconds();
+
+    $('#nowTime')
+        .append("<div>" + leadingZeros(H,2) + "<span>hrs</span></div>")
+        .append("<div>" + leadingZeros(M,2) + "<span>min</span></div>")
+        .append("<div>" + leadingZeros(S,2) + "<span>sec</span></div>");
+
+
+}, 1000);
